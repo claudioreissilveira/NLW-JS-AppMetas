ca3c74d3d1932e0fa97913193e28038041b387c4
claudioreissilveira <texasreyli@gmail.com>
2024-09-11 23:33:23 -0300

    Cadastrar meta

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const { select } = require("@inquirer/prompts");
+const { select, input } = require("@inquirer/prompts");
+
+let meta = {
+  value: "Tomar 3L de agua por dia",
+  checked: false,
+};
+let metas = [meta];
+
+const cadastrarMeta = async () => {
+  const meta = await input({ message: "Digite a meta:" });
+
+  if (meta.length == 0) {
+    console.log("A meta nao pode ser vazia");
+    return;
+  }
+
+  metas.push({ value: meta, checked: false });
+};
 
 const start = async () => {
   while (true) {
@@ -11,7 +28,7 @@ const start = async () => {
         },
         {
           name: "Listar metas",
-          value: "listar"
+          value: "listar",
         },
         {
           name: "Sair",
@@ -22,13 +39,14 @@ const start = async () => {
 
     switch (opcao) {
       case "cadastrar":
-        console.log("vamos cadastrar");
+        await cadastrarMeta();
+        console.log(metas);
         break;
       case "listar":
         console.log("vamos listar");
         break;
       case "sair":
-        console.log("Até a próxima!")
+        console.log("Até a próxima!");
         return;
     }
   }
